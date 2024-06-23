00b8f01727a8cf91aa995080db1591575df49bc0
audrey06lee05 <audrey06lee05@gmail.com>
2024-06-23 06:15:14 +0100

    added Link to Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,1 +1,21 @@
 // src/components/Navbar.js
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <h1>My Store</h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/cart">Cart</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
