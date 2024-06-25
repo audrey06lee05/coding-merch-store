9a6405670bab463cd36c80db00d768af877bf63d
audrey06lee05 <audrey06lee05@gmail.com>
2024-06-25 01:57:02 +0100

    added product images in homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,28 +1,57 @@
 // src/pages/HomePage.js
 import React from "react";
 import { Link } from "react-router-dom";
+import product1Image from "../Img/SWE_black_hoodie.JPG";
+import product2Image from "../Img/SWE_navy_hoodie.JPG";
+import product3Image from "../Img/FEBE_black_hoodie_front.JPG";
+import product4Image from "../Img/FEBE_grey_hoodie_front.JPG";
+import product5Image from "../Img/FEBE_navy_hoodie_front.JPG";
+import product6Image from "../Img/SWE_black_tshirt.JPG";
+import product7Image from "../Img/FEBE_black_tshirt_front.JPG";
 
 // product list
 const products = [
   {
     id: 1,
-    name: "Software Engineer Unisex Hoodie",
+    name: "Black - Software Engineer Unisex Hoodie",
     price: 64.99,
+    img: product1Image,
   },
   {
     id: 2,
-    name: "Front-end Back-end Developer Unisex Hoodie",
+    name: "Navy - Software Engineer Unisex Hoodie",
     price: 64.99,
+    img: product2Image,
   },
   {
     id: 3,
-    name: "Software Engineer Unisex T-shirt",
-    price: 44.99,
+    name: "Black - Front-end Back-end Developer Unisex Hoodie",
+    price: 64.99,
+    img: product3Image,
   },
   {
     id: 4,
-    name: "Front-end Back-end Unisex T-shirt",
+    name: "Grey - Front-end Back-end Developer Unisex Hoodie",
+    price: 64.99,
+    img: product4Image,
+  },
+  {
+    id: 5,
+    name: "Navy - Front-end Back-end Developer Unisex Hoodie",
+    price: 64.99,
+    img: product5Image,
+  },
+  {
+    id: 6,
+    name: "Black - Software Engineer Unisex T-shirt",
     price: 44.99,
+    img: product6Image,
+  },
+  {
+    id: 7,
+    name: "Black - Front-end Back-end Unisex T-shirt",
+    price: 44.99,
+    img: product7Image,
   },
 ];
 
@@ -34,10 +63,11 @@ const HomePage = () => {
       <div>
         {products.map((product) => (
           <div key={product.id}>
-            <a>
-              <img src=""></img>
-            </a>
             <Link to={`/product/${product.id}`}>
+              <img
+                src={product.img}
+                style={{ width: "200px", height: "auto" }}
+              />
               <h3>{product.name}</h3>
             </Link>
             <p>USD{product.price}</p>
