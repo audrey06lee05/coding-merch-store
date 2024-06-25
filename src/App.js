14b129b1b71732c5c656776eb67aabfc3f05ab44
audrey06lee05 <audrey06lee05@gmail.com>
2024-06-25 04:22:34 +0100

    added contact and about pages in app.js and added the route for them

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
+import AboutPage from "./pages/About";
+import ContactPage from "./pages/Contact";
 import CartPage from "./pages/CartPage";
 import Navbar from "./components/Navbar";
 
 //check components
 console.log(HomePage);
 console.log(ProductPage);
+console.log(AboutPage);
+console.log(ContactPage);
 console.log(CartPage);
 console.log(Navbar);
 
@@ -19,6 +23,8 @@ function App() {
         <Route path="/" exact component={HomePage} />
         <Route path="/product/:id" component={ProductPage} />{" "}
         {/* id for specifying multiple products*/}
+        <Route path="/about" component={AboutPage} />
+        <Route path="/contact" component={ContactPage} />
         <Route path="/cart" component={CartPage} />
       </Switch>
     </Router>
